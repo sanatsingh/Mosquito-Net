fa9810ebbbe90543aaba2064ce74b6026c86d354
Sanat B Singh <1705068@kiit.ac.in>
2021-03-07 14:03:13 +0530

    feat: More models added to public repository

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-# Mosquito-Net
+# Malaria Detection (Models & Web App)
+Malaria is a mosquito-borne life-threatening disease caused by five species of Plasmodium parasites (single-celled organisms).   
+
+## Mosquito-Net
 A deep learning based lightweight Computer Aided Diagnostic System with an aim of easing the weary task of detection of Malaria infected cells by examination of blood smears under microscope and to enable the practical use of the model in remote and rural areas where high computation power and network may or may not be available.
 
-Public Repository in spirit of upcoming *World Malaria Day* - 25th April.
+Public Repository in spirit of *World Malaria Day* - 25th April.
 
-Two models have been provided with different activation fuctions (ReLU and Mish) & fc layers.
+## Mosquito-Net (Mish version)
+Mosquito net with a different activation function [Mish](https://github.com/digantamisra98/Mish). Slightly improved performance.  
 
-Live Model can be found at - https://zyik.ml
+## Mosquito-Net V2 (Mobile version)
+A more optimized version of MosquitoNet with Depthwise Seperable Convolutions for mobile devices accepted & presented in[*ICML 2020, MLGH Workshop Poster*](https://drive.google.com/file/d/1OoCqThpsm9N38eUTGNmxwRhVat8IxWVl/view)
 
-## About Malaria
-Malaria is a mosquito-borne life-threatening disease caused by five species of Plasmodium parasites (single-celled organisms). 
 
-## Note
+### Note
 As COVID-19 spreads rapidly around the globe, there is an urgent need to aggressively tackle the novel coronavirus while ensuring that other killer diseases, such as malaria, are not neglected. WHO urges countries to ensure the continuity of malaria services in the context of the pandemic, provided that best practices to protect health workers and communities are followed. Find more at https://www.who.int/malaria/areas/epidemics_emergencies/covid-19/en/
--- model_st.py
+++ model_st.py
@@ -113,3 +113,92 @@ class MosquitoNet_Mish(nn.Module):
         
         return out
 
+
+class DepthwiseSepConv(nn.Module):
+    def __init__(self, inch, ch, kernel_size=3, stride=1, dilation=1, bias=True, BatchNorm=True):
+        '''
+        DepthwiseSepConv Module implemented in PyTorch. Divided in 2 parts:-
+        1) Depth Wise Convolution : using nn.Conv2d layer with groups parameter each kernel is passed for a single layer of input
+        2) Point Wise Convolution : 1x1 Convolution implemented using nn.Conv2d layer
+
+        BatchNorm (optional) available, requires to pass the function/module to be used.
+
+        '''
+        super(DepthwiseSepConv, self).__init__()
+        self.depthconv = nn.Conv2d(
+            inch, inch, kernel_size, stride, 0, dilation, groups=inch, bias=bias)
+        self.btn = nn.BatchNorm2d(inch)
+        self.pointconv = nn.Conv2d(inch, ch, 1, 1, 0, 1, 1, bias=bias)
+        self.dilation = dilation
+        self.kernel_size = kernel_size
+        self.BatchNorm = BatchNorm
+
+    def padding_fix(self, x, kernel_size, dilation):
+        kernel_size_effective = kernel_size + \
+            (kernel_size - 1) * (dilation - 1)
+        pad_total = kernel_size_effective - 1
+        pad_beg = pad_total // 2
+        pad_end = pad_total - pad_beg
+        padded_inputs = F.pad(x, (pad_beg, pad_end, pad_beg, pad_end))
+        return padded_inputs
+
+    def forward(self, x):
+        x = self.padding_fix(x, self.kernel_size, self.dilation)
+        x = self.depthconv(x)
+        if not self.BatchNorm:
+            x = self.btn(x)
+        x = self.pointconv(x)
+        return x
+
+class MosquitoNet_V2(nn.Module):
+    ''' Another version of MosquitoNet using Mish activation function, outperforms Original Version'''
+
+    def __init__(self):
+        super(MosquitoNet_V2, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            DepthwiseSepConv(3, 16, kernel_size=5, stride=1),
+            Mish(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.layer2 = nn.Sequential(
+            DepthwiseSepConv(16, 32, kernel_size=3, stride=1),
+            Mish(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.layer3 = nn.Sequential(
+            DepthwiseSepConv(32, 64, kernel_size=3, stride=1),
+            Mish(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.layer4 = nn.Sequential(
+            DepthwiseSepConv(64, 128, kernel_size=3, stride=1),
+            Mish(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.fc1 = nn.Linear(128*7*7, 4096)
+        self.fc2 = nn.Linear(4096, 2048)
+        self.fc3 = nn.Linear(2048, 2)
+        self.drop = nn.Dropout2d(0.2)
+        self.m = Mish()
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        # flatten out a input for Dense Layer
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.m(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.m(out)
+        out = self.drop(out)
+        out = self.fc3(out)
+
+        return out
